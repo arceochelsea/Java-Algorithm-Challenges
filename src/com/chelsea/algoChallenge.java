726c7aad1e369da074aec1dd6439b56921f3f3b2
Chelsea Arceo <arceo.chelsea@gmail.com>
2020-11-11 23:36:16 -0500

    Alternative to if else

--- src/com/chelsea/algoChallenge.java
+++ src/com/chelsea/algoChallenge.java
@@ -16,24 +16,49 @@ public class algoChallenge {
         int secondNum = Integer.parseInt(lastStr);
         //System.out.println("second num: "+ secondNum);
 
-        if (operator.equals("x") || operator.equals("X") || operator.equals("*")) {
-            int totalProduct = firstNum * secondNum;
-            System.out.println(totalProduct);
-        } else if (operator.equals("-")) {
-            int totalDifference = firstNum - secondNum;
-            System.out.println(totalDifference);
-        } else if (operator.equals("/")) {
-            float totalQuotient = (float) firstNum / secondNum;
-            System.out.println(totalQuotient);
-        } else if (operator.equals("%")) {
-            int totalRemainder = firstNum % secondNum;
-            System.out.println(totalRemainder);
-        } else if (operator.equals("+")) {
-            int totalSum = firstNum + secondNum;
-            System.out.println(totalSum);
-        } else {
-            System.out.println("invalid operator! try again");
-        }
+//        if (operator.equals("x") || operator.equals("X") || operator.equals("*")) {
+//            int totalProduct = firstNum * secondNum;
+//            System.out.println(totalProduct);
+//        } else if (operator.equals("-")) {
+//            int totalDifference = firstNum - secondNum;
+//            System.out.println(totalDifference);
+//        } else if (operator.equals("/")) {
+//            float totalQuotient = (float) firstNum / secondNum;
+//            System.out.println(totalQuotient);
+//        } else if (operator.equals("%")) {
+//            int totalRemainder = firstNum % secondNum;
+//            System.out.println(totalRemainder);
+//        } else if (operator.equals("+")) {
+//            int totalSum = firstNum + secondNum;
+//            System.out.println(totalSum);
+//        } else {
+//            System.out.println("invalid operator! try again");
+//        }
 
+        //alternative to long if else statement... is a long switch case statement...
+        switch (operator) {
+            case "x": case "X": case "*":
+                int totalProduct = firstNum * secondNum;
+                System.out.println(totalProduct);
+                break;
+            case "+":
+                int totalSum = firstNum + secondNum;
+                System.out.println(totalSum);
+                break;
+            case "-":
+                int totalDifference = firstNum - secondNum;
+                System.out.println(totalDifference);
+                break;
+            case "/":
+                float totalQuotient = (float) firstNum / secondNum;
+                System.out.println(totalQuotient);
+                break;
+            case "%":
+                int totalRemainder = firstNum % secondNum;
+                System.out.println(totalRemainder);
+                break;
+            default:
+                System.out.println("invalid operator! try again");
+        }
     }
 }
